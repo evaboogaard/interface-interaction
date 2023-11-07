a3df838f971f7b270ff0b670bc8dda38642f079b
Eva Boogaard <evaboogaard@hotmail.com>
2023-11-07 14:00:42 +0100

    (style): add content

--- scripts/planets.js
+++ scripts/planets.js
@@ -1,15 +1,17 @@
-const planetOne = document.querySelector('.planets button:first-of-type');
-const planetOneSection = document.querySelector('.planets .baby');
-const planetOneSectionStripeOne = document.querySelector(
-    '.planets .baby .stripe-1'
-);
-const planetOneSectionStripeTwo = document.querySelector(
-    '.planets .baby .stripe-2'
-);
+const planetOneButton = document.querySelector('.planets button:first-of-type');
+const planetOneSection = document.querySelector('.planets .childhood');
 
-planetOne.addEventListener('click', () => {
-    planetOne.classList.toggle('clicked');
+planetOneButton.addEventListener('click', () => {
+    planetOneButton.classList.toggle('clicked');
     planetOneSection.classList.toggle('shown');
-    planetOneSectionStripeOne.classList.toggle('shown');
-    planetOneSectionStripeTwo.classList.toggle('shown');
+});
+
+const planetTwoButton = document.querySelector(
+    '.planets button:nth-of-type(2)'
+);
+const planetTwoSection = document.querySelector('.planets .teen');
+
+planetTwoButton.addEventListener('click', () => {
+    planetTwoButton.classList.toggle('clicked');
+    planetTwoSection.classList.toggle('shown');
 });
