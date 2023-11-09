11b3622d14a44ed380a6170c7a83354d245ceaf3
Eva Boogaard <evaboogaard@hotmail.com>
2023-11-09 15:33:51 +0100

    (fix): clean up sass files

--- scripts/trip.js
+++ scripts/trip.js
@@ -7,9 +7,11 @@ tripAudio.loop = true;
 tripButton.addEventListener('click', () => {
     if (documentEl.classList.contains('trip')) {
         tripAudio.pause();
+        tripButton.classList.remove('clicked');
         documentEl.classList.remove('trip');
     } else {
         documentEl.classList.add('trip');
+        tripButton.classList.add('clicked');
         tripAudio.play();
     }
 });
